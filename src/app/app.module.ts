c191a6a3fd51a3c66c58f7e0d7868aa260d90f16
itsVGK <gkrishgkrish@gmail.com>
2019-08-05 09:50:39 +0530

    5/8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeetingViewComponent } from './meeting-view/meeting-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeetingViewComponent
   ],
   imports: [
     BrowserModule,
