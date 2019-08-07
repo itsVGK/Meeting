78135af00a3216c894ca72ec765afc0ad41dd6c5
itsVGK <gkrishgkrish@gmail.com>
2019-08-07 10:17:39 +0530

    7/8

--- src/app/meeting-view/meeting-view.component.ts
+++ src/app/meeting-view/meeting-view.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeetingViewComponent implements OnInit {
 
-  public dates:Array<any>=[1,2,3,4,5,6,7];
+  public dates:Array<any>=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
   public rows:Array<any>=[1,2,3];
   constructor() { }
 
